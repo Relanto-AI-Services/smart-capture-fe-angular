875c6fefa0fa061c23051f6302a15c539c3e197f
vidya sagar jha <vidyasagar.jha@relanto.ai>
2025-04-08 15:41:26 +0530

    risk assessment

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -16,27 +16,20 @@ export class ApiService {
     tacticDetails: Array<any>
   ): Observable<any> {
     const url = `${this.baseUrl}/save_spend_request_tactic`;
-
     const sessionId = localStorage.getItem('sid') || ''; 
-
     const headers = new HttpHeaders({
       accept: 'application/json',
       'session-id': sessionId,
       'Content-Type': 'application/json',
     });
-
     const body = {
       spend_request_id: spendRequestId,
       tactic_details: tacticDetails,
     };
-
     return this.http.post(url, body, { headers });
   }
 
 
-  // getSpendRequestData(): Observable<any> {
-  //   return this.http.get<any>( `${this.baseUrl}/tactic_listing`);
-  // }
 
   getSpendRequest() {
     const sessionIdd = localStorage.getItem('sid') || '';
@@ -55,4 +48,26 @@ export class ApiService {
 
     return this.http.get( `${this.baseUrl}/tactics_refresh`, { headers });
   }
+
+  saveRisk(
+    spendRequestId: string,
+    sowForm: {},
+    riskfields: any 
+    
+  ): Observable<any> {
+    const url = `${this.baseUrl}/save_risk_fields`;
+    const sessionId = localStorage.getItem('sid') || ''; 
+    const headers = new HttpHeaders({
+      accept: 'application/json',
+      'session-id': sessionId,
+      'Content-Type': 'application/json',
+    });
+    const body = {
+      spend_request_id: spendRequestId,
+      risk_form: riskfields,  
+      sow_form: sowForm
+    };
+    return this.http.post(url, body, { headers });
+  }
+
 }
